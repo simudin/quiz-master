46ef87320c60ce71035f4c1d722cf0603f40b720
Hasim Sofaruddin <harudin91@gmail.com>
2016-09-10 06:53:34 +0700

    add game over

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,16 +1,25 @@
 class FeaturesController < ApplicationController
-  before_filter :set_question, only: [:quiz]
+  before_action :set_question, only: [:quiz]
+  before_action :get_live, except: [:dashboard]
   def dashboard
+    session[:lives] = 3
+    get_live
   end
 
   def quiz
   end
 
   def score
+  end
 
+  def game_over
   end
 
   private
+  def get_live
+    @lives = session[:lives]
+  end
+
   def set_question
     @question = params[:id].present? ? Question.find(params[:id]) : Question.first
   end
